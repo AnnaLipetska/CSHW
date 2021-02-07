e49b6ca49c0d1323ab698b5498d4866070809cff
Anna <a.lipetska@gmail.com>
2021-02-07 14:01:55 +0200

    $ - Input/Output homework added

---
+++ CSHW/Task7_1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_1
+{
+    // Используя Visual Studio, создайте проект по шаблону Console Application.
+    // Создайте программу, в которой создайте на диске 50 директорий с именами
+    // от Folder_0 до Folder_50, после чего вывести информацию на консоль
+    // о каждой из директорий, затем удалите их.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            string lastDriveName = string.Empty;
+            for (int i = drives.Count() - 1; i >= 0; i--)
+            {
+                if (drives[i].DriveType == DriveType.CDRom)
+                {
+                    continue;
+                }
+                lastDriveName = (drives[i]).Name;
+                break;
+            }
+
+            DirectoryInfo directory = new DirectoryInfo(lastDriveName);
+
+            if (directory.Exists)
+            {
+                var directoryInfos = new List<DirectoryInfo>();
+                for (int i = 0; i < 50; i++)
+                {
+                    directoryInfos.Add(directory.CreateSubdirectory("Folder_" + i));
+                }
+
+                foreach (var directoryInfo in directoryInfos)
+                {
+                    Console.WriteLine(string.Format(@"Директория {0} успешно создана", directoryInfo.Name));
+                    Console.Write(Environment.NewLine);
+
+                    Console.WriteLine("FullName    : {0}", directoryInfo.FullName);
+                    Console.WriteLine("Name        : {0}", directoryInfo.Name);
+                    Console.WriteLine("Parent      : {0}", directoryInfo.Parent);
+                    Console.WriteLine("CreationTime: {0}", directoryInfo.CreationTime);
+                    Console.WriteLine("Attributes  : {0}", directoryInfo.Attributes.ToString());
+                    Console.WriteLine("Root        : {0}", directoryInfo.Root);
+
+                    Console.Write(Environment.NewLine);
+                    Console.WriteLine(new string('-', 30));
+                }
+
+                Console.ReadKey();
+
+                for (int i = 0; i < 50; i++)
+                {
+                    try
+                    {
+                        Directory.Delete($"{lastDriveName}Folder_{i}", true);
+                        Console.WriteLine($"Каталог Folder_{i} успешно удален.");
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ CSHW/Task7_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_2
+{
+    // Используя Visual Studio, создайте проект по шаблону Console Application.
+    // Создайте программу, в которой создайте файл, запишите в него произвольные данные и закройте файл.
+    // Затем снова откройте этот файл, прочитайте из него данные и выведете их на консоль.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter writer = File.CreateText("MyFile.txt");
+            writer.WriteLine("Произвольные данные");
+            writer.Close();
+
+            Console.ReadKey();
+
+            StreamReader reader = File.OpenText("MyFile.txt");
+            Console.WriteLine(reader.ReadToEnd());
+
+            Console.ReadKey();
+        }
+    }
+}
